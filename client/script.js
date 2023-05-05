c37e885c21c14617c4e239a24fa1374a685de22f
KobiManuel <Emmakobi91@gmail.com>
2023-05-05 17:52:17 +0100

    feat(User Interface): changed all images and icons

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
-import bot from "./assets/bot.svg";
-import user from "./assets/user.svg";
+import bot from "./assets/lunabot1.png";
+import user from "./assets/user.png";
 
 const form = document.querySelector("form");
 const textarea = document.querySelector("textarea");
